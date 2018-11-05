f2d768331f11c6ed24de9d648f6af0974dc91784
Mike Wilding <mwilding@fuelmedical.com>
2018-11-05 12:13:33 -0800

    adds various structs based on todo

--- router/maslow.go
+++ router/maslow.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type managed_device struct {
+	Name             string   `json: "name"`
+	Mac_address      string   `json: "mac_address"`
+	Default_policies []policy `json: "default_policies"`
+	Id               int      `json: "id"`
+}
+
+func (m *managed_device) addPolicy() err {
+
+}
+
+type policy struct {
+	Name string
+}
+
+type site struct {
+	Name          string
+	Servers_regex []string
+}
+
 func main() {
-	fmt.Println("vim-go")
 }
