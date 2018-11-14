47960cfa3a7c5b9b505f7005f85821dcc19baa6b
Mike Wilding <michaelwchastain@gmail.com>
2018-11-14 07:55:45 -0800

    adds cobra command cli, basic rootCmd and addsite command

--- router/maslow.go
+++ router/maslow.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	firebase "firebase.google.com/go"
+	"github.com/spf13/cobra"
 	//"firebase.google.com/go/auth"
 	"fmt"
 	"golang.org/x/net/context"
@@ -29,30 +30,48 @@ type Site struct {
 	Servers_regex []string `json: "servers_regex"`
 }
 
+var addsite = &cobra.Command{
+	Use:   "addsite",
+	Short: "add a site to db",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("inside the addsite cobra command")
+		ctx := context.Background()
+
+		config := &firebase.Config{
+			DatabaseURL: "https://maslow-test.firebaseio.com",
+		}
+		opt := option.WithCredentialsFile("../secrets/maslow-test-firebase-adminsdk-dhcpw-61989a0f5b.json")
+		app, err := firebase.NewApp(ctx, config, opt)
+		if err != nil {
+			log.Fatalf("error initializing app: %v\n", err)
+		}
+		client, err := app.Database(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		site := Site{"Netfrlix", []string{"siteregex1", "siteregex2"}}
+		if err := client.NewRef("sites").Set(ctx, site); err != nil {
+			log.Fatal(err)
+		}
+		resp := Site{}
+		if err := client.NewRef("sites").Get(ctx, &resp); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(resp.Name)
+	},
+}
+
+func init() {
+
+}
 func main() {
+	rootCmd := &cobra.Command{
+		Use:   "maslow",
+		Short: "a web traffic shaper",
+		Long:  "Think of all the wonderful things you will be able to do with your time",
+	}
+	rootCmd.AddCommand(addsite)
 
-	ctx := context.Background()
-
-	config := &firebase.Config{
-		DatabaseURL: "https://maslow-test.firebaseio.com",
-	}
-	opt := option.WithCredentialsFile("../secrets/maslow-test-firebase-adminsdk-dhcpw-61989a0f5b.json")
-	app, err := firebase.NewApp(ctx, config, opt)
-	if err != nil {
-		log.Fatalf("error initializing app: %v\n", err)
-	}
-	client, err := app.Database(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	site := Site{"Netflix", []string{"siteregex1", "siteregex2"}}
-	if err := client.NewRef("sites").Set(ctx, site); err != nil {
-		log.Fatal(err)
-	}
-	resp := Site{}
-	if err := client.NewRef("sites").Get(ctx, &resp); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(resp.Name)
+	rootCmd.Execute()
 }
