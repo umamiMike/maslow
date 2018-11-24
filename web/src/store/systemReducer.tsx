f95b6d1f041f8f8213e6b7e7f27def49642b00ee
Peter Banka <pbanka@fastly.com>
2018-11-24 15:30:14 -0800

    🎨 clean up test data

--- web/src/store/systemReducer.tsx
+++ web/src/store/systemReducer.tsx
@@ -1,21 +1,6 @@
 import { DeviceState } from "../interfaces";
 
-const initState = {
-  devices: [
-    {
-      id: "1",
-      name: "glyph",
-      description: "Peter's iPhone",
-      mac: "00:50:f1:80:00:00"
-    },
-    {
-      id: "2",
-      name: "zorro",
-      description: "wireless scale",
-      mac: "dc:a9:04:76:06:c2"
-    }
-  ]
-};
+const initState = { devices: [] };
 
 interface Action {
   type: string;
