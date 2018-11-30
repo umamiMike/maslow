66bd823d164a8bd5834eb6d86b30578b07ecd49c
Peter Banka <pbanka@fastly.com>
2018-11-29 22:17:59 -0800

    🎨 Factor Card

--- web/src/device/DeviceSummary.tsx
+++ web/src/device/DeviceSummary.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { DeviceType, PolicyDict, UserDict } from "../interfaces";
 import UserGravatar from "../components/UserGravatar";
+import Card from "../components/Card";
 
 const DeviceSummary = ({
   device,
@@ -12,27 +13,17 @@ const DeviceSummary = ({
   policyDict: PolicyDict;
 }) => {
   return (
-    <div className="max-w w-full lg:flex p-1 border-r bg-blue-lightest border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light ">
-      <div
-        className="h-48 lg:h-auto lg:w-48 bg-blue-lightest flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-        style={{
-          backgroundImage: `url('/${device.model}.png')`
-        }}
-        title="Device"
-      />
-      <div className="lg:w-64 bg-blue-lightest p-4 flex flex-col justify-between leading-normal">
-        <div className="text-black font-bold text-xl mb-2">{device.name}</div>
-        <p className="text-grey-darker text-base">{device.description}</p>
-        <p className="text-grey">{device.mac}</p>
-        <p className="text-grey">
-          {`Default policy: ${policyDict[device.defaultPolicyId].name}`}
-        </p>
-        {device.users.map(userId => {
-          const user = userDict[userId] || {};
-          return <UserGravatar key={userId} user={user} userId={userId} />;
-        })}
-      </div>
-    </div>
+    <Card backgroundImage={device.model} alt="Device" title={device.name}>
+      <p className="text-grey-darker text-base">{device.description}</p>
+      <p className="text-grey">{device.mac}</p>
+      <p className="text-grey">
+        {`Default policy: ${policyDict[device.defaultPolicyId].name}`}
+      </p>
+      {device.users.map(userId => {
+        const user = userDict[userId] || {};
+        return <UserGravatar key={userId} user={user} userId={userId} />;
+      })}
+    </Card>
   );
 };
 
