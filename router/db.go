748116f4f7ff660e02420c3724b361612df03dc1
Mike Wilding <michaelwchastain@gmail.com>
2018-12-01 08:33:51 -0800

    adds stub arp.go; db.go and moves firebase conn, cobra command in main creating map[string]interface and send to add site function
    adds refactored main maslow.go

---
+++ router/db.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	firebase "firebase.google.com/go"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
+	"log"
+	//"firebase.google.com/go/auth"
+	//"fmt"
+)
+
+var config = &firebase.Config{
+	DatabaseURL: "https://maslow-test.firebaseio.com",
+}
+
+func makeConnectionAndAddSite(d map[string]interface{}) string {
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("../secrets/maslow-test-firebase-adminsdk-dhcpw-61989a0f5b.json")
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	_, _, err = client.Collection("users").Add(ctx, d)
+	if err != nil {
+		log.Fatal(err)
+		return "failure"
+	}
+	return "success"
+}
--- router/maslow.go
+++ router/maslow.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	firebase "firebase.google.com/go"
+	"fmt"
 	"github.com/spf13/cobra"
-	//"firebase.google.com/go/auth"
-	//"fmt"
-	"golang.org/x/net/context"
-	"google.golang.org/api/option"
-	"log"
 )
 
 type managed_device struct {
@@ -16,9 +11,6 @@ type managed_device struct {
 	Default_policies []policy `json: "default_policies"`
 	Id               int      `json: "id"`
 }
-
-//func (m *managed_device) addPolicy() err {
-//}
 
 type policy struct {
 	Name string
@@ -35,30 +27,20 @@ var rootCmd = &cobra.Command{
 	Long:  "Think of all the wonderful things you will be able to do with your time",
 }
 
-var config = &firebase.Config{
-	DatabaseURL: "https://maslow-test.firebaseio.com",
-}
-
 var addsite = &cobra.Command{
 	Use:   "addsite",
 	Short: "add a site to db",
 	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		opt := option.WithCredentialsFile("../secrets/maslow-test-firebase-adminsdk-dhcpw-61989a0f5b.json")
-		app, err := firebase.NewApp(ctx, config, opt)
-		if err != nil {
-			log.Fatalf("error initializing app: %v\n", err)
+
+		data := map[string]interface{}{
+			"first": "Nada",
+			"last":  "Surf",
+			"born":  2045,
 		}
-		client, err := app.Firestore(ctx)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer client.Close()
-		_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
-			"first": "Ada",
-			"last":  "Lovelace",
-			"born":  1815,
-		})
+
+		message := makeConnectionAndAddSite(data)
+		fmt.Println(message)
+
 	},
 }
 
