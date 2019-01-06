2857dd833d20637919968813a5f478b11c1c7902
Mike Wilding <michaelwchastain@gmail.com>
2019-01-05 16:17:57 -0800

    adds var for testpoint, ✨ update executeBatch to exit prior to attempting command if "usr/sbin/iptables" does not exist

--- router/iptables.go
+++ router/iptables.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -22,7 +23,6 @@ type set map[string]struct{}
  */
 func generateWhitelist(dnsMap map[string][]string, hostMap map[string]host, devicePolicies devicePolicyMap) map[string][]string {
 	// TODO: Add failsafe to www.maslowsystem.com
-
 	output := make(map[string][]string)
 	for mac, addressRegexList := range devicePolicies {
 		ipAddress := hostMap[mac].IP
@@ -31,7 +31,6 @@ func generateWhitelist(dnsMap map[string][]string, hostMap map[string]host, devi
 			continue
 		}
 		fmt.Println("Generating whiteList for", mac)
-		// forbiddenIPs := []string{}
 		forbiddenIPs := set{}
 		for _, addressRegex := range addressRegexList {
 			r, _ := regexp.Compile(addressRegex)
@@ -57,14 +56,19 @@ func makeIPTablesRules(whitelist map[string][]string) []string {
 	}
 	for localIP, remoteIPList := range whitelist {
 		for _, remoteIP := range remoteIPList {
-			output = append(output, fmt.Sprintf("-A %s -s %s -d %s -j ACCEPT", ruleName, localIP, remoteIP))
+			val := fmt.Sprintf("-A %s -s %s -d %s -j ACCEPT", ruleName, localIP, remoteIP)
+			output = append(output, val)
 		}
 	}
 	output = append(output, fmt.Sprintf("-A %s -j REJECT", ruleName))
 	return output
 }
-
 func executeBatch(commands []string) {
+	_, err := os.Stat(ipTables)
+	if err != nil {
+		log.Println(ipTables + " does not exist")
+	}
+	os.Exit(1)
 	for _, command := range commands {
 		log.Printf("Running command and waiting for it to finish...%s", command)
 		parts := strings.Split(command, " ")
