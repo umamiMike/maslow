494c3d41764cbf0efc14132cfc214dbf4f7f9956
Michael Wilding <michaelwchastain@gmail.com>
2019-02-07 11:10:39 -0800

    :white_check_mark: add unit test using assert from ->  :heavy_plus_sign: adds testify lib

--- router/dnslog_test.go
+++ router/dnslog_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 var line1 = `Dec 31 16:01:44 dnsmasq[1301]: reading /tmp/resolv.dnsmasq `
 var line2 = `Dec 31 16:01:45 dnsmasq[1301]: reply 2.pool.ntp.org is 45.127.112.2`
+var mockLog = "../test/logs/dnsmasq.log"
 
 //testing parseLog used in the readAndParseDns fn
 func TestParseLog(t *testing.T) {
@@ -24,6 +27,7 @@ func TestParseLog(t *testing.T) {
 
 	//returns correctly formatted data
 }
+
 func TestLine2(t *testing.T) {
 
 	key, val, err := parseLog(line2)
@@ -37,5 +41,16 @@ func TestLine2(t *testing.T) {
 
 	if val != "45.127.112.2" {
 		t.Errorf("val should be an ip address")
+	}
+}
+
+func TestReadAndParseDNS(t *testing.T) {
+	//make a file in the file system (remember to remove it)
+	nonexistantfile := "nonexistant"
+	_, err := readAndParseDNS(nonexistantfile)
+	if err != nil {
+		expected := fmt.Sprintf("open %v: no such file or directory", nonexistantfile)
+		//t.Errorf("%v", err)
+		assert.Equal(t, expected, err.Error(), "the error message is not correct")
 	}
 }
