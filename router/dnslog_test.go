593b5da3e0fd3a79732a3d40c409b659db417197
Michael Wilding <michaelwchastain@gmail.com>
2019-02-18 17:55:13 -0800

    :construction: writing unit tests for ParseLog

--- router/dnslog_test.go
+++ router/dnslog_test.go
@@ -6,13 +6,16 @@ import (
 	"testing"
 )
 
-var line1 = `Dec 31 16:01:44 dnsmasq[1301]: reading /tmp/resolv.dnsmasq `
 var mockLog = "../test/logs/dnsmasq.log"
 
 //testing parseLog used in the readAndParseDns fn
 func TestParseLog(t *testing.T) {
 
-	key, val, err := parseLog(line1)
+	mocklinefromfile := `Dec 31 16:01:44 dnsmasq[1301]: reading /tmp/resolv.dnsmasq `
+	key, val, err := parseLog(mocklinefromfile)
+	fmt.Printf("the val of key is: %v", key)
+
+	//fmt.Printf("%v %v %v \n ", key, val, err)
 	if err != nil {
 		t.Errorf("key or val should be empty")
 	}
@@ -27,18 +30,26 @@ func TestParseLog(t *testing.T) {
 	//returns correctly formatted data
 }
 
+//parseLog
+
+// will return unreadable line if  no colon in line
+// will return 2 blank strings if  reply
+// will return 2 blank strings if CNAME
+//parseLog  will  ruturn string string, err
+//test
 func TestParseLog2(t *testing.T) {
+	unreadable := `I have no colon, so I am not logged line`
+	_, _, err := parseLog(unreadable)
+	assert.Error(t, err, "the line should not be readable")
 
+	//edge case of reply
 	line := `Dec 31 16:01:45 dnsmasq[1301]: reply 2.pool.ntp.org is 45.127.112.2`
-	key, val, err := parseLog(line)
-	if err != nil {
-		t.Errorf("key and val should be should dns , and val should be ip")
-	}
 
-	assert.Equal(t, key, "2.pool.ntp.org", "key should be a dns entry")
-	if val != "45.127.112.2" {
-		t.Errorf("val should be an ip address")
-	}
+	key, val, err := parseLog(line)
+
+	assert.Nil(t, err, "if reply exists ip")
+	assert.Equal(t, key, "", "key is blank")
+	assert.Equal(t, val, "", "key is blank")
 }
 
 func TestReadAndParseDNS(t *testing.T) {
