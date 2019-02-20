3aab883275b1af330afbff4b1d3978b086ab53c8
Michael Wilding <michaelwchastain@gmail.com>
2019-02-20 13:19:20 -0800

    :construction: upates dnslog_test.go

--- router/dnslog_test.go
+++ router/dnslog_test.go
@@ -8,51 +8,7 @@ import (
 
 var mockLog = "../test/logs/dnsmasq.log"
 
-//testing parseLog used in the readAndParseDns fn
-func TestParseLog(t *testing.T) {
-
-	mocklinefromfile := `Dec 31 16:01:44 dnsmasq[1301]: reading /tmp/resolv.dnsmasq `
-	key, val, err := parseLog(mocklinefromfile)
-	fmt.Printf("the val of key is: %v", key)
-
-	//fmt.Printf("%v %v %v \n ", key, val, err)
-	if err != nil {
-		t.Errorf("key or val should be empty")
-	}
-	if key != "" {
-		t.Errorf("key should be dns entry")
-	}
-
-	if val != "" {
-		t.Errorf("val should be an ip address")
-	}
-
-	//returns correctly formatted data
-}
-
-//parseLog
-
-// will return unreadable line if  no colon in line
-// will return 2 blank strings if  reply
-// will return 2 blank strings if CNAME
-//parseLog  will  ruturn string string, err
-//test
-func TestParseLog2(t *testing.T) {
-	unreadable := `I have no colon, so I am not logged line`
-	_, _, err := parseLog(unreadable)
-	assert.Error(t, err, "the line should not be readable")
-
-	//edge case of reply
-	line := `Dec 31 16:01:45 dnsmasq[1301]: reply 2.pool.ntp.org is 45.127.112.2`
-
-	key, val, err := parseLog(line)
-
-	assert.Nil(t, err, "if reply exists ip")
-	assert.Equal(t, key, "", "key is blank")
-	assert.Equal(t, val, "", "key is blank")
-}
-
-func TestReadAndParseDNS(t *testing.T) {
+func TestReadAndParseDNSNoFile(t *testing.T) {
 	//make a file in the file system (remember to remove it)
 	nonexistantfile := "nonexistant"
 	_, err := readAndParseDNS(nonexistantfile)
@@ -61,4 +17,33 @@ func TestReadAndParseDNS(t *testing.T) {
 		//t.Errorf("%v", err)
 		assert.Equal(t, expected, err.Error(), "the error message is not correct")
 	}
+}
+
+//testing parseLog used in the readAndParseDns fn
+func TestParseLog(t *testing.T) {
+
+	mocklinefromfile := `Dec 31 16:01:44 dnsmasq[1301]: reading /tmp/resolv.dnsmasq `
+	key, val, err := parseLog(mocklinefromfile)
+	fmt.Printf("the val of key is: %v", key)
+
+	assert.Nil(t, err, "there should be no error")
+	assert.Empty(t, key, "the key should be empty")
+	assert.Empty(t, val, "the val should be empty")
+}
+
+func TestParseLogBadLine(t *testing.T) {
+	badline := `I have no colon, so I am not logged line`
+	_, _, err := parseLog(badline)
+	assert.Error(t, err, "the line should not be readable")
+}
+
+func TestParseLogReply(t *testing.T) {
+	//edge case of reply
+	line := `Dec 31 16:01:45 dnsmasq[1301]: reply 2.pool.ntp.org is 45.127.112.2`
+
+	key, val, err := parseLog(line)
+
+	assert.Nil(t, err, "if reply exists")
+	assert.Equal(t, key, "2.pool.ntp.org", "key is a dns site")
+	assert.Equal(t, val, "45.127.112.2", "val is an ip")
 }
