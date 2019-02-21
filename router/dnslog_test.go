c20799dbcd72650c60e897e36e012588277b6026
Michael Wilding <michaelwchastain@gmail.com>
2019-02-20 17:22:29 -0800

    :white_check_mark: adds  benchmark tests spike :construction:  and a make target

--- router/dnslog_test.go
+++ router/dnslog_test.go
@@ -19,10 +19,28 @@ func TestReadAndParseDNSNoFile(t *testing.T) {
 	}
 }
 
+func TestReadAndParseDNS(t *testing.T) {
+	testfile := "../test/logs/dnsmasq.log"
+	ret, _ := readAndParseDNS(testfile)
+	fmt.Println(len(ret["pixiedust.buzzfeed.com"]))
+}
+
 //testing parseLog used in the readAndParseDns fn
 func TestParseLog(t *testing.T) {
 
 	mocklinefromfile := `Dec 31 16:01:44 dnsmasq[1301]: reading /tmp/resolv.dnsmasq `
+	key, val, err := parseLog(mocklinefromfile)
+	fmt.Printf("the val of key is: %v", key)
+
+	assert.Nil(t, err, "there should be no error")
+	assert.Empty(t, key, "the key should be empty")
+	assert.Empty(t, val, "the val should be empty")
+}
+
+//test parseLog to deal with encountering CNAMEs
+func TestParseLogCname(t *testing.T) {
+
+	mocklinefromfile := `Jan 13 19:49:44 dnsmasq[1301]: reply raw.githubusercontent.com is <CNAME>`
 	key, val, err := parseLog(mocklinefromfile)
 	fmt.Printf("the val of key is: %v", key)
 
@@ -46,4 +64,19 @@ func TestParseLogReply(t *testing.T) {
 	assert.Nil(t, err, "if reply exists")
 	assert.Equal(t, key, "2.pool.ntp.org", "key is a dns site")
 	assert.Equal(t, val, "45.127.112.2", "val is an ip")
+}
+func BenchmarkReadAndParse(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		testfile := "../test/logs/dnsmasq.log"
+		readAndParseDNS(testfile)
+	}
+
+}
+func BenchmarkParseLog(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		line := `Dec 31 16:01:45 dnsmasq[1301]: reply 2.pool.ntp.org is 45.127.112.2`
+
+		parseLog(line)
+	}
+
 }
